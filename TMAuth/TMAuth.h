4022c246d96d4108b0d7625a10dc43466dfa2dc6
Jason Tsai <tez125@g.ncu.edu.tw>
2017-04-20 01:39:18 +0800

    Initial Commit

---
+++ TMAuth/TMAuth.h
@@ -0,0 +1,19 @@
+//
+//  TMAuth.h
+//  TMAuth
+//
+//  Created by Jason Tsai on 2017/4/20.
+//  Copyright © 2017年 YomStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TMAuth.
+FOUNDATION_EXPORT double TMAuthVersionNumber;
+
+//! Project version string for TMAuth.
+FOUNDATION_EXPORT const unsigned char TMAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TMAuth/PublicHeader.h>
+
+
